5641869f4d9f50a2ab4dcdf4758d79cae6b2d803
Micheal kuo <sxdcfv20002000@gmail.com>
2021-10-12 10:22:14 +0800

    “first“

---
+++ src/components/Summary.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+function Summary(props) {
+  const { productCount, total } = props
+  const [selectedOption, setSelectedOption] = useState('')
+
+  return (
+    <>
+      <div className="col-md-4 summary">
+        <div>
+          <h5>
+            <b>付款摘要</b>
+          </h5>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col col-style">共 {productCount} 項目</div>
+        </div>
+        <section id="selectedOption">
+          <h2>下拉選單</h2>
+          <select
+            value={selectedOption}
+            onChange={(e) => {
+              setSelectedOption(e.target.value)
+            }}
+          >
+            {/* 第一個值會用state的初始值 */}
+            <option value="">請選擇</option>
+            <option value="200">郵局 $+200</option>
+            <option value="250">快遞 $+250</option>
+          </select>
+        </section>
+        <div className="row row-style">
+          <div className="col">總價</div>
+          <div className="col text-right">${total + selectedOption * 1}</div>
+        </div>
+        <button className="btn">前往付款</button>
+      </div>
+    </>
+  )
+}
+
+export default Summary
