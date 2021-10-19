797d4fdf3f89ef991cd25374704bc53dd8584bd6
Micheal kuo <sxdcfv20002000@gmail.com>
2021-10-19 16:33:59 +0800

    “1019PM0430-1“

--- src/components/homework3/App.js
+++ src/components/homework3/App.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react'
-import DatePicker from 'react-datepicker'
+import React, { Component } from 'react'
 
+import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
-function App() {
-  const [startDate, setStartDate] = useState(new Date())
+class MyDatepickerComponent extends Component {
+  state = {
+    startDate: new Date(),
+  }
 
-  return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-  )
+  handleChange = (date) => {
+    // this.setState({
+    //   startDate: date,
+    // })
+    if (new Date().getFullYear() - date.getFullYear() > 18) {
+      alert('恭喜成年')
+    } else {
+      alert('小屁孩')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <DatePicker
+          onChange={this.handleChange}
+          selected={this.state.startDate}
+          dateFormat="MM/dd/yyyy"
+        />
+      </div>
+    )
+  }
 }
 
-export default App
+export default MyDatepickerComponent
